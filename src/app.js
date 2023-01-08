1f53f9cee8025c1aaf02b4d9fc5c11159801a80b
AstronomAlpaca <stevenrockett@gmail.com>
2023-01-08 15:30:49 +0000

    app entry starts with event listener for a new game. game state and UI is initialised

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
 import "./styles/app.css";
+import { initialiseGame } from "./modules/game-controller";
+import { initialiseUI } from "./modules/DOM-controller";
+
+const newGameBtn = document.querySelector("#newGameBtn");
+
+newGameBtn.addEventListener("click", function () {
+  initialiseGame();
+  initialiseUI();
+});
