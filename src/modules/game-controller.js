56f338f2448ca74dfd919c81812c93b450ee1493
AstronomAlpaca <stevenrockett@gmail.com>
2023-01-08 15:27:37 +0000

    initialise human and CPU scores to 0 upon new game

---
+++ src/modules/game-controller.js
@@ -0,0 +1,11 @@
+let humanScore;
+let cpuScore;
+
+function initialiseGame() {
+  humanScore = 0;
+  cpuScore = 0;
+}
+
+function increaseScore(player) {}
+
+export { initialiseGame, humanScore, cpuScore };
